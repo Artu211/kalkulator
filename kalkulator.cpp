8b3c669ffd74b8fef7860e96df58208d47cdae19
Artu211 <160549068+Artu211@users.noreply.github.com>
2024-02-26 17:27:06 +0100

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int a, b, wynik;
+char znak;
+int main()
+{
+while(cin>>znak>>a>>b){
+    switch(znak){
+        case '+':
+          wynik=a+b;
+          break;
+
+        case '-':
+        wynik=a-b;
+        break;
+
+        case '':
+        wynik=a*b;
+        break;
+
+        case '/':
+        wynik=a/b;
+        break;
+
+        case '%':
+        wynik=a%b;
+        break;
+    }
+    cout<<wynik<<endl;
+}
+    return 0;
+}
